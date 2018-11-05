fe84576244e41abc27c4cde7cfb7551dded35663
Johan Sylvan <gtc124@gmail.com>
2018-11-05 08:37:43 +0100

    Fixed env in a place

--- src/components/RouteList/index.js
+++ src/components/RouteList/index.js
@@ -10,7 +10,7 @@ class RouteList extends Component {
               <li key={route.id}>
                 api/[
                 <a
-                  href={`${process.env.REACH_APP_API}/api/${route.route}`}
+                  href={`${process.env.REACT_APP_API}/api/${route.route}`}
                   target="_blank"
                   rel="noopener noreferrer"
                 >
