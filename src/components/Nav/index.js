ac87936eaf54adc15b0d0cd5982d25876ec8271c
Johan Sylvan <gtc124@gmail.com>
2018-11-14 10:31:51 +0100

    Some margin

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,12 +5,12 @@ import Octicons, {Database, Home} from '@githubprimer/octicons-react'
 class Nav extends Component {
     render() {
         return (
-            <div className="col-1">
+            <div className="col-1 mt-4">
                 <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                     <Link to="/" className="nav-link" id="v-pills-home-tab" data-toggle="pill" role="tab" aria-controls="v-pills-home" aria-selected="true">
                         <Octicons size="medium" icon={Home}></Octicons>
                     </Link>
-                    <Link to="/databases" className="nav-link" id="v-pills-profile-tab" data-toggle="pill" role="tab" aria-controls="v-pills-profile" aria-selected="false">
+                    <Link to="/databases" className="nav-link ml-1" id="v-pills-profile-tab" data-toggle="pill" role="tab" aria-controls="v-pills-profile" aria-selected="false">
                         <Octicons size="medium" icon={Database}>
                     </Octicons></Link>
                 </div>
