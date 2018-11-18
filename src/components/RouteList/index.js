b04baf756d1f0e44693a0f7511d3aee48f37375a
Johan Sylvan <gtc124@gmail.com>
2018-11-18 01:51:24 +0100

    Accordion

--- src/components/RouteList/index.js
+++ src/components/RouteList/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react"
 
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+} from 'react-accessible-accordion'
+
 import RegisterRoute from '../RegisterRoute'
+
+import 'react-accessible-accordion/dist/minimal-example.css';
 
 class RouteList extends Component {
 
@@ -14,37 +23,42 @@ class RouteList extends Component {
 
   removeRoute = id => {
     fetch(`${process.env.REACT_APP_API}/build/remove/route/${id}`)
-    .then(res => this.props.parent.getRoutes())
-    .catch(console.log)
+      .then(res => this.props.parent.getRoutes())
+      .catch(console.log)
   }
 
   render() {
+    console.log(this.props.parent.state.routes);
+    
     return (
-      <div>
+      <div className="container float-left">
         <h5>Routes</h5>
-        <ul className="list-group list-group-flush">
+        <Accordion>
           {this.props.parent.state.routes ? (
             this.props.parent.state.routes.map(route => (
-              <li className="list-group-item" key={route.id}>
-                api/
-                <a
-                  href={`${process.env.REACT_APP_API}/api/${route.route}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {route.route}
-                </a>
-                <span> : </span>
-                to {route.method}{this.action(route.action).limit ? ` ${this.action(route.action).limit}` : " all"} rows of "{this.action(route.action).toReturn.join(", ")}" from table "{this.action(route.action).from}"
-                <button className="btn btn-danger btn-sm float-right" onClick={() => this.removeRoute(route.id)}>X</button>
-              </li>
+              <div className="card" key={route.id}>
+                <AccordionItem className="card-body">
+                  <AccordionItemTitle className="card-title">
+                    <h3 style={{ display: "inline" }}>api/{route.route}</h3>
+                    <button className="btn btn-danger btn-sm float-right" onClick={() => this.removeRoute(route.id)}>X</button>
+                  </AccordionItemTitle>
+                  <AccordionItemBody className="card-text">
+                    <p>Url: <a href={`${process.env.REACT_APP_API}/api/${route.route}`}
+                      target="_blank"
+                      rel="noopener noreferrer">{`${process.env.REACT_APP_API}/api/${route.route}`}</a></p>
+                    <p>Method: {route.method}</p>
+                    <p>Table: {this.action(route.action).from}</p>
+                    <p>Columns: {this.action(route.action).toReturn.join(", ")}</p>
+                  </AccordionItemBody>
+                </AccordionItem>
+              </div>
             ))
           ) : (
-            <li>No routes</li>
-          )}
-        </ul>
-        <button className="btn btn-default" onClick={() => this.setState({addNew: !this.state.addNew})}>{this.state.addNew ? "Cancel" : "Add new"}</button>
-        { this.state.addNew && <RegisterRoute update={this.props.parent.getRoutes} database={this.props.parent.state.info[0].id} routes={this.props.parent.state.routes}></RegisterRoute>}
+              <li>No routes</li>
+            )}
+        </Accordion>
+        <button className="btn btn-default" onClick={() => this.setState({ addNew: !this.state.addNew })}>{this.state.addNew ? "Cancel" : "Add new"}</button>
+        {this.state.addNew && <RegisterRoute update={this.props.parent.getRoutes} database={this.props.parent.state.info[0].id} routes={this.props.parent.state.routes}></RegisterRoute>}
       </div>
     )
   }
